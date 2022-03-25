1ee6467094a1bf59a3721ceb685a526eaedfcf2d
Kate Miháliková <kate@katemihalikova.cz>
2022-03-25 19:20:54 +0100

    chore(demo): move main import to app module, as per readme

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -7,10 +7,37 @@ import { TranslateDemoModule } from './translate-demo/translate-demo.module';
 import { TemporalDemoModule } from './temporal-demo/temporal-demo.module';
 import { MomentDemoModule } from './moment-demo/moment-demo.module';
 
+import { IzCoreModule, IzLocaleDefinition } from '@ization/core';
+import { IzTranslateModule } from '@ization/translate';
+import { IzTemporalModule } from '@ization/temporal';
+import { IzMomentModule } from '@ization/moment';
+
+const LOCALES: IzLocaleDefinition[] = [
+  {localeCode: 'cs-CZ', translateLocaleCode: 'cs'}, // Czech
+  {localeCode: 'cs-x-japan', translateLocaleCode: 'cs', temporalLocaleCode: 'cs-CZ-u-ca-japanese', momentLocaleCode: 'cs'}, // Czech with Japanese imperial calendar via custom locale code
+  {localeCode: 'ja'}, // Japanese
+  {localeCode: 'ja-JP-u-ca-japanese', translateLocaleCode: 'ja'}, // Japanese with Japanese imperial calendar
+  {localeCode: 'en-GB', translateLocaleCode: 'en'}, // English with British formatting
+  {localeCode: 'en-US'}, // English with American formatting
+];
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+
+    IzCoreModule.withLocales(LOCALES),
+    IzTranslateModule.withConfig({
+      fallbackLocaleCode: 'en',
+      translationLoader: async locale => await import(
+        /* webpackInclude: /\.js$/ */
+        /* webpackChunkName: "translations-[request]" */
+        `../translations/${locale}`
+      ).catch(() => ({})),
+    }),
+    IzTemporalModule,
+    IzMomentModule,
+
     CoreDemoModule,
     TemporalDemoModule,
     TranslateDemoModule,
